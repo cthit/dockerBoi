a53c189d151d43f746c8ea9616a2a9751a84381c
Joakim Hulthe <joakim@hulthe.net>
2021-07-03 01:09:05 +0200

    Prettier cards & add copy to clipboard fn

--- frontend/src/page/repo.rs
+++ frontend/src/page/repo.rs
@@ -3,7 +3,7 @@
 use crate::registry::{self, RepoName};
 use seed::browser::fetch;
 use seed::prelude::*;
-use seed::{div, error, h2, p, C};
+use seed::{div, error, h2, a, C};
 
 pub struct Model {
     repo: RepoName,
@@ -13,6 +13,12 @@ pub struct Model {
 #[derive(Debug)]
 pub enum Msg {
     FetchedTags(fetch::Result<Vec<String>>),
+    CopyLink(String),
+}
+
+
+fn copy_to_clipboard(text: &str) {
+    seed::window().navigator().clipboard().write_text(text);
 }
 
 pub fn init(repo: RepoName, orders: &mut impl Orders<Msg>) -> Model {
@@ -31,11 +37,24 @@ pub fn update(msg: Msg, model: &mut Model, _orders: &mut impl Orders<Msg>) {
                 error!(e);
             }
         },
+        Msg::CopyLink(text) => copy_to_clipboard(&text),
     }
 }
 
 pub fn view(model: &Model) -> Node<Msg> {
-    let view_card = |tag: &String| div![C!["repo_card"], p![C!["repo_card_header"], tag]];
+
+    let view_card = |tag: &String| {
+        let link: String = format!("localhost:8080/{}:{}", model.repo,tag);
+        div![
+            C!["repo_card"],
+            a![C!["repo_card_header"], tag],
+            a![
+                C!["repo_link"],
+                link.clone(),
+                ev(Ev::Click, |_| Msg::CopyLink(link)),
+            ]
+        ]
+    };
     div![
         h2![C!["repo-name"], &model.repo.to_string()],
         model.tags.iter().map(view_card)
