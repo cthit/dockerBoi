1cb52aef966f01f781776bd274cc424c1f1f789e
Joakim Hulthe <joakim@hulthe.net>
2021-08-18 23:38:51 +0200

    Add basic support for issue token auth

--- frontend/src/registry.rs
+++ frontend/src/registry.rs
@@ -1,5 +1,6 @@
-use seed::browser::fetch::{self, fetch};
+use seed::browser::fetch::{self, fetch, header::Header, Request, Response};
 use serde::Deserialize;
+use std::collections::HashMap;
 use std::fmt::{self, Display, Formatter};
 use std::str::FromStr;
 
@@ -15,18 +16,64 @@ pub async fn get_repositories() -> fetch::Result<Vec<RepoName>> {
         repositories: Vec<String>,
     }
 
-    async {
-        let response: RegistryResponse =
-            fetch("/v2/_catalog").await?.check_status()?.json().await?;
+    let response: RegistryResponse = get_with_token("/v2/_catalog")
+        .await?
+        .check_status()?
+        .json()
+        .await?;
 
-        Ok(response
-            .repositories
-            .into_iter()
-            .map(|name| RepoName::from_str(&name))
-            .collect::<Result<Vec<_>, _>>()
-            .expect("todo"))
+    Ok(response
+        .repositories
+        .into_iter()
+        .map(|name| RepoName::from_str(&name))
+        .collect::<Result<Vec<_>, _>>()
+        .expect("todo"))
+}
+
+pub async fn get_with_token(url: &str) -> fetch::Result<Response> {
+    #[derive(Deserialize)]
+    struct TokenResponse {
+        token: String,
     }
-    .await
+
+    let maybe_response = fetch(url).await?;
+    let status = maybe_response.status();
+    if status.code == 401 {
+        let www_auth = maybe_response
+            .raw_response()
+            .headers()
+            .get("www-authenticate");
+        let www_auth = match www_auth {
+            Ok(Some(www_auth)) => www_auth,
+            _ => todo!("handle error"),
+        };
+
+        let www_auth = www_auth.strip_prefix("Bearer ").unwrap();
+
+        let params: HashMap<_, _> = www_auth
+            .split(",")
+            .flat_map(|kv| kv.split_once("="))
+            .map(|(k, v)| (k, v.trim_matches('"')))
+            .collect();
+
+        let realm = *params.get("realm").unwrap();
+
+        let token: TokenResponse = fetch(format!(
+            "{}?service={}&scope={}",
+            realm,
+            params.get("service").unwrap(),
+            params.get("scope").unwrap()
+        ))
+        .await?
+        .check_status()?
+        .json()
+        .await?;
+
+        let request = Request::new(url).header(Header::bearer(token.token));
+        fetch(request).await
+    } else {
+        Ok(maybe_response)
+    }
 }
 
 pub async fn get_image_tags(name: RepoName) -> fetch::Result<Vec<String>> {
@@ -38,7 +85,7 @@ pub async fn get_image_tags(name: RepoName) -> fetch::Result<Vec<String>> {
     }
 
     async {
-        let response: RegistryResponse = fetch(format!("/v2/{}/tags/list", name))
+        let response: RegistryResponse = get_with_token(&format!("/v2/{}/tags/list", name))
             .await?
             .check_status()?
             .json()
